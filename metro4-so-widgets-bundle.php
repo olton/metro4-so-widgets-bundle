bf2821ab5205d025601ca7394bb9a85776958aa5
Sergey Pimenov <sergey@pimenov.com.ua>
2018-11-20 20:01:00 +0200

    initial commit

---
+++ metro4-so-widgets-bundle.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Metro 4 SiteOrigin Widgets Bundle
+Description: A collection of Metro 4 widgets for Site Origin Page Builder.
+Version: 1.0.0
+Text Domain: metro4-so-widgets-bundle
+Domain Path: /lang
+Author: Sergey Pimenov
+Author URI: https://metroui.org.ua
+Plugin URI: https://github.com/metro4-so-widgets-bundle/
+License: MIT
+License URI: https://github.com/metro4-so-widgets-bundle/LICENSE
+*/
